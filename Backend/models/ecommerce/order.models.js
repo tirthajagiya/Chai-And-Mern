adb5ada16adeab2623a8071df276b9a7624b8b34
tirthajagiya <tirthajagiya5618@gmail.com>
2024-12-15 23:39:41 +0530

    new

--- Backend/models/ecommerce/order.models.js
+++ Backend/models/ecommerce/order.models.js
@@ -1,14 +1,36 @@
 import mongoose from "mongoose";
 
-const orderSchema=new mongoose.Schema({
-    orderPrice:{
-        type:Number,
-        required:true                
+const orderItemSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
     },
-    customer:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
+    quantity: {
+        type: Number,
+        required: true
     }
-},{timestamps:true});
+})
+const orderSchema = new mongoose.Schema({
+    orderPrice: {
+        type: Number,
+        required: true
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    OrderItems: {
+        type: [orderItemSchema]
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["PENDING" ,"CANCELLED" ,"DELIVERED"],
+        default:"PENDING"
+    }
+}, { timestamps: true });
 
-export const Order=mongoose.model("Order",orderSchema);
+export const Order = mongoose.model("Order", orderSchema);
